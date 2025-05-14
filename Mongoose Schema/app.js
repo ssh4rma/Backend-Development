19d9218a93129e5e48b32d92c8d1535d0a44bef8
Shubham <shubham.sharma4390@gmail.com>
2025-05-14 11:00:34 +0530

    created mongoose schema

--- Mongoose Schema/app.js
+++ Mongoose Schema/app.js
@@ -1,4 +1,30 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:2701/demoDB')
+mongoose.connect('mongodb://localhost:27017/demoDB')
   .then(() => console.log('Successfully connected'))
-  .catch(() => console.log('Error connecting'))
+  .catch(() => console.log('Error connecting'));
+
+const demoSchema = new mongoose.Schema({
+  name: {
+    type: String, 
+    require: true
+  },
+  phone: {
+    type: Number, 
+    require: false
+  },
+  email: {
+    type: String,
+    require: true
+  }
+})
+
+//make a mongoose model
+const demo = new mongoose.model('Demo', demoSchema); 
+
+const d1 = new demo ({
+  name: 'Shubham', 
+  phone: 9112341343,
+  email: 'shubham@gmail.com'
+});
+
+d1.save();
